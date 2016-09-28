b55dfa875b6e448e6e174ef61e5a8c79e0c573e7
jwaldrip <jason@waldrip.net>
2016-09-27 23:46:45 -0600

    config fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+# Set defaults
+export VAULT_PORT=${VAULT_PORT-8200}
+[[ "$VAULT_LISTENER_TCP_TLS_DISABLE" = "true" ]] && scheme="http" || scheme="https"
+export VAULT_ADDR=${VAULT_ADDR-"$scheme://$(hostname -i):$VAULT_PORT"}
+
+# Functions
 config() {
   if ! echo "$@" | grep "\-config" > /dev/null ; then
     if ! getpcaps $$ 2>&1 | grep -o cap_ipc_lock > /dev/null ; then
@@ -8,8 +14,8 @@ config() {
       >&2 echo "         Running without mlock is not recommended for production."
       export VAULT_DISABLE_MLOCK=true
     fi
-    VAULT_PORT=${VAULT_PORT-8200}
-    export VAULT_LISTENER_TCP_ADDRESS=${VAULT_LISTENER_TCP_ADDRESS-$(hostname -i):$VAULT_PORT}
+    export VAULT_REDIRECT_ADDR=${VAULT_REDIRECT_ADDR-"$scheme://$(hostname -i):$VAULT_PORT"}
+    export VAULT_LISTENER_TCP_ADDRESS=${VAULT_LISTENER_TCP_ADDRESS-0.0.0.0:$VAULT_PORT}
     confd -onetime
     sed -i '/^\s*$/d' /usr/share/vault.hcl
     echo "-config /usr/share/vault.hcl $@"
